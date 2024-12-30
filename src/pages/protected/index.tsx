eceaad4a49d791161e167005ca8b443d8105ac84
Jose Cruz <jose.cruzal@outlook.com>
2024-12-30 08:28:52 -0500

    change route

--- src/pages/protected/index.tsx
+++ src/pages/protected/index.tsx
@@ -6,9 +6,9 @@ import { TasksPage } from './Tasks';
 export const PrivateRoutes = () => {
     return (
         <Routes>
+            <Route path='*' element={<Navigate to='/' replace />} />
             <Route path='/' element={<HomePage />} />
             <Route path='tasks' element={<TasksPage />} />
-            <Route path='*' element={<Navigate to='/' replace />} />
         </Routes>
     );
 };
